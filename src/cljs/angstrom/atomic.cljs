334218ab654d43bfc652b12c4756cae9f94e37a8
ryan spangler <ryan.spangler@gmail.com>
2013-10-24 15:08:35 -0700

    ported angstrom three.js fully over to clojurescript

--- src/cljs/angstrom/atomic.cljs
+++ src/cljs/angstrom/atomic.cljs
@@ -2,258 +2,293 @@
 
 (def default-radius 1.0)
 
+(defn object
+  [m]
+  (let [obj (js/Object.)]
+    (doseq [[k v] m]
+      (aset obj (name k) v))
+    obj))
+
 (def elements
-  {:H {:name "Hydrogen"
-       :number 1
-       :color 0xffffff
-       :radius 0.23}
+  (let [all {:H {:name "Hydrogen"
+                 :number 1
+                 :color 0xffffff
+                 :radius 0.23}
 
-   :Li {:name "Lithium"
-        :number 3
-        :color 0xb22222
-        :radius default-radius}
+             :Li {:name "Lithium"
+                  :number 3
+                  :color 0xb22222
+                  :radius default-radius}
 
-   :Be {:name "Beryllium"
-        :number 4
-        :radius default-radius}
+             :Be {:name "Beryllium"
+                  :number 4
+                  :radius default-radius}
 
-   :B {:name "Boron"
-       :number 5
-       :color 0x00ff00
-       :radius 0.83}
+             :B {:name "Boron"
+                 :number 5
+                 :color 0x00ff00
+                 :radius 0.83}
 
-   :C {:name "Carbon"
-       :number 6
-       :color 0xc8c8c8
-       :radius 0.68}
+             :C {:name "Carbon"
+                 :number 6
+                 :color 0xc8c8c8
+                 :radius 0.68}
 
-   :N {:name "Nitrogen"
-       :number 7
-       :color 0x8f8fff
-       :radius 0.68}
+             :N {:name "Nitrogen"
+                 :number 7
+                 :color 0x8f8fff
+                 :radius 0.68}
 
-   :O {:name "Oxygen"
-       :number 8
-       :color 0xf00000
-       :radius 0.68}
+             :O {:name "Oxygen"
+                 :number 8
+                 :color 0xf00000
+                 :radius 0.68}
 
-   :F {:name "Fluorine"
-       :number 9
-       :color 0xdaa520
-       :radius 0.64}
+             :F {:name "Fluorine"
+                 :number 9
+                 :color 0xdaa520
+                 :radius 0.64}
 
-   :Mg {:name "Magnesium"
-        :number 12
-        :radius default-radius}
+             :Mg {:name "Magnesium"
+                  :number 12
+                  :radius default-radius}
 
-   :Al {:name "Aluminium"
-        :number 13
-        :color 0x808090
-        :radius default-radius}
+             :Al {:name "Aluminium"
+                  :number 13
+                  :color 0x808090
+                  :radius default-radius}
 
-   :Si {:name "Silicon"
-        :number 14
-        :color 0xdaa520
-        :radius 1.20}
+             :Si {:name "Silicon"
+                  :number 14
+                  :color 0xdaa520
+                  :radius 1.20}
 
-   :P {:name "Phosphorus"
-       :number 15
-       :color 0xffa500
-       :radius 1.05}
+             :P {:name "Phosphorus"
+                 :number 15
+                 :color 0xffa500
+                 :radius 1.05}
 
-   :S {:name "Sulfur"
-       :number 16
-       :color 0xffc832
-       :radius 1.02}
+             :S {:name "Sulfur"
+                 :number 16
+                 :color 0xffc832
+                 :radius 1.02}
 
-   :Cl {:name "Chlorine"
-        :number 17
-        :color 0x00ff00
-        :radius 0.99}
+             :Cl {:name "Chlorine"
+                  :number 17
+                  :color 0x00ff00
+                  :radius 0.99}
 
-   :Ar {:name "Argon"
-        :number 18
-        :radius default-radius}
+             :Ar {:name "Argon"
+                  :number 18
+                  :radius default-radius}
 
-   :V {:name "Vanadium"
-       :number 23
-       :radius default-radius}
+             :V {:name "Vanadium"
+                 :number 23
+                 :radius default-radius}
 
-   :Cr {:name "Chromium"
-        :number 24
-        :color 0x808090
-        :radius default-radius}
+             :Cr {:name "Chromium"
+                  :number 24
+                  :color 0x808090
+                  :radius default-radius}
 
-   :Mn {:name "Manganese"
-        :number 25
-        :color 0x808090
-        :radius default-radius}
+             :Mn {:name "Manganese"
+                  :number 25
+                  :color 0x808090
+                  :radius default-radius}
 
-   :Co {:name "Cobalt"
-        :number 27
-        :radius default-radius}
+             :Co {:name "Cobalt"
+                  :number 27
+                  :radius default-radius}
 
-   :Cu {:name "Copper"
-        :number 29
-        :color 0xa52a2a
-        :radius default-radius}
+             :Cu {:name "Copper"
+                  :number 29
+                  :color 0xa52a2a
+                  :radius default-radius}
 
-   :Zn {:name "Zinc"
-        :number 30
-        :color 0xa52a2a
-        :radius default-radius}
+             :Zn {:name "Zinc"
+                  :number 30
+                  :color 0xa52a2a
+                  :radius default-radius}
 
-   :Ga {:name "Gallium"
-        :number 31
-        :radius default-radius}
+             :Ga {:name "Gallium"
+                  :number 31
+                  :radius default-radius}
 
-   :As {:name "Arsenic"
-        :number 33
-        :radius 1.21}
+             :As {:name "Arsenic"
+                  :number 33
+                  :radius 1.21}
 
-   :Se {:name "Selenium"
-        :number 34
-        :radius 1.22}
+             :Se {:name "Selenium"
+                  :number 34
+                  :radius 1.22}
 
-   :Br {:name "Bromine"
-        :number 35
-        :color 0xa52a2a
-        :radius 1.21}
+             :Br {:name "Bromine"
+                  :number 35
+                  :color 0xa52a2a
+                  :radius 1.21}
 
-   :Kr {:name "Krypton"
-        :number 36
-        :radius default-radius}
+             :Kr {:name "Krypton"
+                  :number 36
+                  :radius default-radius}
 
-   :Rb {:name "Rubidium"
-        :number 37
-        :radius default-radius}
+             :Rb {:name "Rubidium"
+                  :number 37
+                  :radius default-radius}
 
-   :Sr {:name "Strontium"
-        :number 38
-        :radius default-radius}
+             :Sr {:name "Strontium"
+                  :number 38
+                  :radius default-radius}
 
-   :Y {:name "Yttrium"
-       :number 39
-       :radius default-radius}
+             :Y {:name "Yttrium"
+                 :number 39
+                 :radius default-radius}
 
-   :Mo {:name "Molybdenum"
-        :number 42
-        :radius default-radius}
+             :Mo {:name "Molybdenum"
+                  :number 42
+                  :radius default-radius}
 
-   :Ag {:name "Silver"
-        :number 47
-        :color 0x808090
-        :radius default-radius}
+             :Ag {:name "Silver"
+                  :number 47
+                  :color 0x808090
+                  :radius default-radius}
 
-   :Cd {:name "Cadmium"
-        :number 48
-        :radius default-radius}
+             :Cd {:name "Cadmium"
+                  :number 48
+                  :radius default-radius}
 
-   :In {:name "Indium"
-        :number 49
-        :radius default-radius}
+             :In {:name "Indium"
+                  :number 49
+                  :radius default-radius}
 
-   :Sb {:name "Antimony"
-        :number 51
-        :radius default-radius}
+             :Sb {:name "Antimony"
+                  :number 51
+                  :radius default-radius}
 
-   :Te {:name "Tellurium"
-        :number 52
-        :radius 1.47}
+             :Te {:name "Tellurium"
+                  :number 52
+                  :radius 1.47}
 
-   :I {:name "Iodine"
-       :number 53
-       :color 0xa020f0
-       :radius 1.40}
+             :I {:name "Iodine"
+                 :number 53
+                 :color 0xa020f0
+                 :radius 1.40}
 
-   :Xe {:name "Xenon"
-        :number 54
-        :radius default-radius}
+             :Xe {:name "Xenon"
+                  :number 54
+                  :radius default-radius}
 
-   :Cs {:name "Caesium"
-        :number 55
-        :radius default-radius}
+             :Cs {:name "Caesium"
+                  :number 55
+                  :radius default-radius}
 
-   :Ba {:name "Barium"
-        :number 56
-        :color 0xffa500
-        :radius default-radius}
+             :Ba {:name "Barium"
+                  :number 56
+                  :color 0xffa500
+                  :radius default-radius}
 
-   :La {:name "Lanthanum"
-        :number 57
-        :radius default-radius}
+             :La {:name "Lanthanum"
+                  :number 57
+                  :radius default-radius}
 
-   :Ce {:name "Cerium"
-        :number 58
-        :radius default-radius}
+             :Ce {:name "Cerium"
+                  :number 58
+                  :radius default-radius}
 
-   :Sm {:name "Samarium"
-        :number 62
-        :radius default-radius}
+             :Sm {:name "Samarium"
+                  :number 62
+                  :radius default-radius}
 
-   :Eu {:name "Europium"
-        :number 63
-        :radius default-radius}
+             :Eu {:name "Europium"
+                  :number 63
+                  :radius default-radius}
 
-   :Gd {:name "Gadolinium"
-        :number 64
-        :radius default-radius}
+             :Gd {:name "Gadolinium"
+                  :number 64
+                  :radius default-radius}
 
-   :Tb {:name "Terbium"
-        :number 65
-        :radius default-radius}
+             :Tb {:name "Terbium"
+                  :number 65
+                  :radius default-radius}
 
-   :Ho {:name "Holmium"
-        :number 67
-        :radius default-radius}
+             :Ho {:name "Holmium"
+                  :number 67
+                  :radius default-radius}
 
-   :Yb {:name "Ytterbium"
-        :number 70
-        :radius default-radius}
+             :Yb {:name "Ytterbium"
+                  :number 70
+                  :radius default-radius}
 
-   :Lu {:name "Lutetium"
-        :number 71
-        :radius default-radius}
+             :Lu {:name "Lutetium"
+                  :number 71
+                  :radius default-radius}
 
-   :W {:name "Tungsten"
-       :number 74
-       :radius default-radius}
+             :W {:name "Tungsten"
+                 :number 74
+                 :radius default-radius}
 
-   :Re {:name "Rhenium"
-        :number 75
-        :radius default-radius}
+             :Re {:name "Rhenium"
+                  :number 75
+                  :radius default-radius}
 
-   :Os {:name "Osmium"
-        :number 76
-        :radius default-radius}
+             :Os {:name "Osmium"
+                  :number 76
+                  :radius default-radius}
 
-   :Ir {:name "Iridium"
-        :number 77
-        :radius default-radius}
+             :Ir {:name "Iridium"
+                  :number 77
+                  :radius default-radius}
 
-   :Pt {:name "Platinum"
-        :number 78
-        :radius default-radius}
+             :Pt {:name "Platinum"
+                  :number 78
+                  :radius default-radius}
 
-   :Au {:name "Gold"
-        :number 79
-        :color 0xdaa520
-        :radius default-radius}
+             :Au {:name "Gold"
+                  :number 79
+                  :color 0xdaa520
+                  :radius default-radius}
 
-   :Hg {:name "Mercury"
-        :number 80
-        :radius default-radius}
+             :Hg {:name "Mercury"
+                  :number 80
+                  :radius default-radius}
 
-   :Tl {:name "Thallium"
-        :number 81
-        :radius default-radius}
+             :Tl {:name "Thallium"
+                  :number 81
+                  :radius default-radius}
 
-   :Pb {:name "Lead"
-        :number 82
-        :radius default-radius}
+             :Pb {:name "Lead"
+                  :number 82
+                  :radius default-radius}
 
-   :U {:name "Uranium"
-       :number 92
-       :radius default-radius}
-   })
+             :U {:name "Uranium"
+                 :number 92
+                 :radius default-radius}}]
+    (into 
+     {}
+     (map 
+      (fn [[sym element]]
+        (let [material (js/THREE.MeshPhongMaterial. (object {:color (or (:color element) 0xff1493)}))
+              geometry (js/THREE.SphereGeometry. (:radius element) 16 16)]
+          [sym (assoc element 
+                 :material material
+                 :geometry geometry)]))
+      all))))
 
+(defn atomic-sphere
+  [atom]
+  (let [element (get elements (:element atom))
+        sphere (js/THREE.Mesh. (:geometry element) (:material element))]
+    (.set (.-position sphere) (:x atom) (:y atom) (:z atom))
+    sphere))
+
+(defn molecular-geometry
+  [atoms]
+  (reduce
+   (fn [molecule segment]
+     (let [node (reduce
+                 (fn [node atom]
+                   (.add node (atomic-sphere atom))
+                   node)
+                 (js/THREE.Object3D.) segment)]
+       (.add molecule node)
+       molecule))
+   (js/THREE.Object3D.) atoms))
--- src/cljs/angstrom/core.cljs
+++ src/cljs/angstrom/core.cljs
@@ -3,46 +3,121 @@
             [angstrom.parse :as parse]
             [angstrom.atomic :as atomic]))
 
-(defrecord Rect [x y width height])
+(def world (atom {}))
 
-(defn rrange
-  [min max]
-  (+ min (rand-int (- max min))))
-
-(defn rprop
-  [min max prop]
-  (rrange (get min prop) (get max prop)))
-
-(defn random-rect
-  ([min max] (random-rect min max 0))
-  ([min max id]
-     (let [rrect (partial rprop min max)]
-       (Rect. (rrect :x) (rrect :y) (rrect :width) (rrect :height)))))
-
-(defn rect-pile
-  [number width height]
-  (let [min (Rect. 0 0 10 10)
-        max (Rect. 500 500 width height)]
-    (map (partial random-rect min max) (range number))))
-
-(defn rect-div!
-  [rect]
-  (let [div (.createElement js/document "div")
-        style (.-style div)]
-    (set! (.-top style) (str (:x rect) "px"))
-    (set! (.-left style) (str (:y rect) "px"))
-    (set! (.-width style) (str (:width rect) "px"))
-    (set! (.-height style) (str (:height rect) "px"))
-    (set! (.-backgroundColor style) (str "rgb(" (rand-int 255) "," (rand-int 255) "," (rand-int 255) ")"))
-    (set! (.-className div) "rect")
-    (.appendChild (.-body js/document) div)))
-
-(defn scatter-rects
+(defn window-size
   []
-  (let [pile (rect-pile 500 100 100)]
-    (doseq [rect pile]
-      (rect-div! rect))))
+  [(.-innerWidth js/window) (.-innerHeight js/window)])
+
+(defn aspect-ratio
+  []
+  (let [[width height] (window-size)]
+    (/ width height)))
+
+(defn init-camera
+  [[x y z] look-at]
+  (let [camera (js/THREE.PerspectiveCamera. 45 (aspect-ratio) 1 10000)]
+    (.set (.-position camera) x y z)
+    (.lookAt camera look-at)
+    camera))
+
+(defn init-renderer
+  []
+  (let [renderer (js/THREE.WebGLRenderer.)
+        [width height] (window-size)]
+    (.setSize renderer width height)
+    (set! (.-shadowMapEnabled renderer) true)
+    (set! (.-shadowMapType renderer) js/THREE.PCFShadowMap)
+    renderer))
+
+(defn init-viewport
+  [renderer]
+  (let [viewport (.getElementById js/document "viewport")]
+    (.appendChild viewport (.-domElement renderer))
+    viewport))
+
+(defn init-lights
+  []
+  (let [ambient (js/THREE.AmbientLight. 0x0011111)
+        point (js/THREE.SpotLight. 0xffffff 1 0 js/Math.PI 1)]
+    (.set (.-position point) -250 250 150)
+    (.set (.-position (.-target point)) 0 0 0)
+    [ambient point]))
+
+(defn init-molecule
+  [scene]
+  (parse/fetch-atoms 
+   :pdb "2dgc" 
+   (fn [atoms]
+     (let [molecule (atomic/molecular-geometry atoms)]
+       (.log js/console molecule)
+       (.add scene molecule)))))
+
+(defn on-key-down [event])
+(defn on-key-up [event])
+(defn on-mouse-down [event])
+(defn on-mouse-move [event])
+
+(defn on-resize
+  []
+  (let [[width height] (window-size)
+        {:keys [camera renderer]} @world]
+    (set! (.-aspect camera) (/ width height))
+    (.updateProjectionMatrix camera)
+    (.setSize renderer width height)))
+
+(defn init-listeners
+  []
+  (.addEventListener js/document "keydown" on-key-down false)
+  (.addEventListener js/document "keyup" on-key-up false)
+  (.addEventListener js/document "mousedown" on-mouse-down false)
+  (.addEventListener js/document "mousemove" on-mouse-move false)
+  (.addEventListener js/window "resize" on-resize false))
+
+(defn init-scene
+  []
+  (let [scene (js/THREE.Scene.)
+        camera (init-camera [0 0 400] (.-position scene))
+        renderer (init-renderer)
+        viewport (init-viewport renderer)
+        controls (js/THREE.OrbitControls. camera)
+        lights (init-lights)]
+    (init-listeners)
+    (doseq [light lights]
+      (.add scene light))
+    (init-molecule scene)
+    {:clock (js/THREE.Clock.)
+     :mouse (js/THREE.Vector2. 0 0)
+     :scene scene
+     :camera camera
+     :renderer renderer
+     :viewport viewport
+     :controls controls
+     :time (.now js/Date)}))
+
+(defn init
+  []
+  (let [scene (init-scene)]
+    (swap! world merge scene)))
+
+(defn render
+  []
+  (let [{:keys [clock time controls renderer scene camera]} @world
+        delta (.getDelta clock)
+        time (+ delta time)]
+    (.update controls)
+    (.render renderer scene camera)
+    (swap! world assoc :time time)))
+
+(defn animate
+  []
+  (.requestAnimationFrame js/window animate)
+  (render))
+
+(set! 
+ (.-onload js/window) 
+ (fn [] 
+   (init)
+   (animate)))
 
 (connect/connect)
-
-(scatter-rects)
